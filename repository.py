9a53d7a1cf322bcc45e2d1b963a85d78123707b1
Gian Pio Domiziani <gianpio.domiziani@consoft.it>
2020-12-18 13:15:55 +0100

    abstract repository started.

--- repository.py
+++ repository.py
@@ -1,14 +1,14 @@
 import abc
-from models.page import Page
+from models.page import ServePages
 
 class AbstractRepository(abc.ABC):
 
     @abc.abstractmethod
-    def add(self, page: Page):
+    def add(self, page: dict):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get(self, id: int) -> Page:
+    def get(self, id: int, ServePages) -> dict:
         raise NotImplementedError
 
 class SQLAlchemyRepository(AbstractRepository):
@@ -16,10 +16,13 @@ class SQLAlchemyRepository(AbstractRepository):
     def __init__(self, session):
         self.session = session
 
-    def add(self, page: Page):
+    def add(self, page):
         self.session.add(page)
     
-    def get(self, id: int) -> Page:
-        return self.session.query()
+    def get(self, id: int, ServePages) -> dict:
+        return self.session.query(ServePages.pages).filter_by(id).one()
+    
+    def list(self):
+        return self.session.query(ServePages.pages).all()
 
 
