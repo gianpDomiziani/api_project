1f4c5cd51cc1026f4651d7175674a8d3acc3e8d0
Gian Pio Domiziani <gianpio.domiziani@consoft.it>
2020-12-18 12:31:25 +0100

    attributes page improved and some property attributes tested.

--- tests/test_page.py
+++ tests/test_page.py
@@ -1,3 +1,5 @@
+
+import pytest
 import os
 import sys
 sys.path.append(
@@ -6,27 +8,28 @@ sys.path.append(
     )
 )
 
-import pytest
 from models.page import Page, ServePages
-pgs_0 = Page(id=1, title='Mars', header='Can Mars hold life?', author='Pino', body='Yes.')
-pgs_1 = Page()
+
+
+pgs_0 = Page(id=0, title='Mars', header='Can Mars hold life?',
+             author='Pino', body='Yes.')
+pgs_1 = Page(id=1)
+
 
 servPages = ServePages()
+
 
 class TestAPI:
 
     @staticmethod
     def test_page():
-        assert pgs_0._id == 1
-        assert pgs_1._id == None
-        assert pgs_0._page
-        assert pgs_1._page 
-        assert len(pgs_0._page.keys()) == 5
-        assert len(pgs_1._page.keys()) == 5
-        assert type(pgs_0._page['body']) == str
-        assert type(pgs_0._page) == dict
-        assert type(pgs_1._page) == dict
-    
+        assert pgs_0.id == 0
+        assert pgs_1.id == 1
+        assert isinstance(pgs_0.page, dict)
+        assert len(pgs_0.page.keys()) == 5
+        assert len(pgs_1.page.keys()) == 5
+        assert type(pgs_0.page['body']) == str
+
     @staticmethod
     def test_pages_are_unique_by_id():
         pgs_a = Page(id=12, title='Luna')
@@ -34,27 +37,29 @@ class TestAPI:
         assert pgs_a != pgs_b
 
     # === ServePages Tests ===
-    
+
     @staticmethod
     def test_servePages_charge():
-        servPages.charge(pgs_1._page)
-        assert servPages._pages[pgs_1._page['id']]
-    
+        assert servPages.totPages == 0
+        servPages.charge(pgs_1.page)
+        assert servPages.totPages == 1
+        assert servPages.pages[pgs_1.page['id']]
+
     @staticmethod
     def test_servePages_get():
-        servPages.charge(pgs_1._page)
-        assert servPages.get(pgs_1._page['id']) != None
+        servPages.charge(pgs_1.page)
+        assert servPages.get(pgs_1.page['id']) != None
         assert servPages.get('Not an ID') == None
-    
+
     @staticmethod
     def test_servePages_delete():
         assert servPages.delete('Not an ID') == False
-        servPages.charge(pgs_1._page)
-        assert servPages.delete(pgs_1._page['id'])
+        servPages.charge(pgs_1.page)
+        assert servPages.delete(pgs_1.page['id'])
 
     @staticmethod
     def test_servePages_modify():
 
         assert servPages.modify(id='Not an ID', field='title') is False
-        servPages.charge(pgs_1._page)
-        assert servPages.modify(pgs_1._page['id'], 'title', 'Modified')
+        servPages.charge(pgs_1.page)
+        assert servPages.modify(pgs_1.page['id'], 'title', 'Modified')
